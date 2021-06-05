ff0ed745b22fcaeb54287a32f2473028446feec0
l08084 <l08084.1989@gmail.com>
2021-06-05 19:26:20 +0900

    test success

--- tests/rules/no-lookbehind-assertions-regexp.test.ts
+++ tests/rules/no-lookbehind-assertions-regexp.test.ts
@@ -2,7 +2,7 @@ import { TSESLint } from '@typescript-eslint/experimental-utils';
 import { lookbehindAssertions } from '../../src/rules/no-lookbehind-assertions-regexp';
 
 const tester = new TSESLint.RuleTester({
-  parser: '@typescript-eslint/parser',
+  parser: require.resolve('@typescript-eslint/parser'),
 });
 
 tester.run('lookbehindAssertions', lookbehindAssertions, {
