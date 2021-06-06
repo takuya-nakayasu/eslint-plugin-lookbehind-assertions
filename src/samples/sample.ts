c04c6db68bc5ae272959b36ab29a5fa644d6a85b
l08084 <l08084.1989@gmail.com>
2021-06-06 17:39:47 +0900

    fix

--- src/samples/sample.ts
+++ src/samples/sample.ts
@@ -7,3 +7,10 @@ let re = new RegExp('(?<=ripe )orange');
 
 ripe_oranges = oranges.filter((fruit) => fruit.match(re));
 console.log(ripe_oranges);
+
+const str = '(?<=ripe )orange';
+
+const re2 = new RegExp(str);
+
+ripe_oranges = oranges.filter((fruit) => fruit.match(re2));
+console.log(ripe_oranges);
